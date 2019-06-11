4a8995cf798e668f02307aba8ccec99f718bb54e
csn95599 <48061067+csn95599@users.noreply.github.com>
2019-06-11 11:51:40 +0800

    Revert "Update spider.py"
    This reverts commit 3678966c5c3a90179329ef4217e8c741b8b277c6.

---
+++ spider.py
@@ -0,0 +1,56 @@
+change
+
+
+
+地方的股份大范甘迪
+
+
+import json
+import timeit
+from multiprocessing import Pool
+import requests
+from requests.exceptions import RequestException
+import re
+
+def get_one_page(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+    except RequestException:
+        return None
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?">(\d+)</i>.*?title="(.*?)".*?img src="(.*?)".*?"star">(.*?)</p>.*?'
+                         +'releasetime">(.*?)</p>.*?"integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>',re.S)
+    items = re.findall(pattern,html)
+    for item in items:
+        yield {
+            'index':item[0],
+            'name':item[1],
+            'image':item[2].strip(),
+            'actor':item[3].strip()[3:],
+            'time':item[4][5:],
+            'score':item[5]+item[6]
+        }
+
+def write_to_file(content):
+    with open('result.txt','a',encoding='utf8') as f:
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
+        f.close()
+
+
+def main(offset):
+    url = 'https://maoyan.com/board/4?offset='+str(offset)
+    html = get_one_page(url)
+    print(html)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    pool.map(main,[i*10 for i in range(10)])
+
+
